306b49b37c813e8300911fb290d1e3c5a0747752
Kevin <kevinjoseph97@gmail.com>
2021-03-03 23:56:27 -0500

    adds login/logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,30 @@ class UsersController < ApplicationController
         erb :'users/show'
     end
 
+    get '/login' do 
+        erb :'/users/login'
+    end
+
+    post '/login' do 
+        # binding.pry
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect "/users/#{user.id}"
+        else
+            erb :'/users/login'
+        end
+
+    end
+
+
+    get '/logout' do 
+        session.clear
+        redirect "/login"
+    end
+
+
+
 end
 
 
