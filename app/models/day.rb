add1d8f27aab9673b01e7bf280a0058feed74b28
Kevin <kevinjoseph97@gmail.com>
2021-03-12 12:51:11 -0500

    validates days

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,7 +1,7 @@
 class Day < ActiveRecord::Base 
     belongs_to :user
     validates_presence_of :day, :workout
-    # validates_acceptance_of :day, :accept => 'Sunday' and 'Monday' and 'Tuesday'
+    validates :day, inclusion: { within: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]}
 end
 
 
